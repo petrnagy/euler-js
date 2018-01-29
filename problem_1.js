5ad879725d1ee8ec53472367e4482f8155dc0da1
Petr Nagy <petr.nagy@outlook.com>
2018-01-29 12:48:38 +0100

    problem_1 v1

---
+++ problem_1.js
@@ -0,0 +1,19 @@
+/**
+ * @see https://projecteuler.net/problem=1
+ * @author PN
+ */
+
+var start = new Date;
+
+var sum = 0.00;
+
+for (var i = 1; i < 1000; i++) {
+    if (0.00 === i % 3 || 0.00 === i % 5) {
+        sum += i;
+    } // end if
+} // end for
+
+var end = new Date;
+
+print("The sum of all the multiples of 3 or 5 below 1000 is: " + sum.toString());
+print("Took: " + (end.getTime() - start.getTime()) / 1000 + 'sec')
